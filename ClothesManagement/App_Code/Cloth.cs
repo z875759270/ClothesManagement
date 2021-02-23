ca7625bb7fb29ead3c5175c0923978f2e6433e41
z875759270 <875759270@qq.com>
2021-02-23 08:55:29 +0800

    -

---
+++ ClothesManagement/App_Code/Cloth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Cloth 的摘要说明
+/// </summary>
+public class Cloth
+{
+    private string id;      //服装编号
+    private string name;    //服装名称
+    private string size;    //服装尺寸
+    private string color;   //服装颜色
+    private string pic;     //服装图片
+    private int count;      //库存
+    private int status;     //服装状态
+
+    public Cloth()
+    {
+        //
+        // TODO: 在此处添加构造函数逻辑
+        //
+    }
+}
---
+++ ClothesManagement/App_Code/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Manager 的摘要说明
+/// </summary>
+public class Manager
+{
+    private string id;          //工号
+    private string name;        //姓名
+    private string pwd;         //密码
+    private int level;          //权限级别
+    private string last_login;  //最后登陆时间
+    private string reg_time;    //注册时间
+    private int status;         //账号状态
+
+    public Manager()
+    {
+        //
+        // TODO: 在此处添加构造函数逻辑
+        //
+    }
+}
