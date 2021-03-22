ed47ded5b18f7f65513a42bcc98849c72a5b6026
z875759270 <875759270@qq.com>
2021-03-22 14:11:23 +0800

    业务逻辑层

---
+++ ClothesManagement/App_Code/Cloth/ClothMis.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// ClothMis 的摘要说明
+/// </summary>
+public class ClothMis
+{
+    public ClothMis()
+    {
+        //
+        // TODO: 在此处添加构造函数逻辑
+        //
+    }
+
+    
+}
