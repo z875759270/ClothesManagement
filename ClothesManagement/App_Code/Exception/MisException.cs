8ceae4a6901e3a4bbdfc5020cb7a166d3aebfb1f
z875759270 <875759270@qq.com>
2021-03-22 14:19:31 +0800

    自定义异常

---
+++ ClothesManagement/App_Code/Exception/MisException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// 自定义异常基类
+/// </summary>
+public class MisException : Exception
+{
+    public MisException(string msg) : base(msg)
+    {
+        
+    }
+
+
+}
